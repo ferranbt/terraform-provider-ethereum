7a21cb7e6088cf7dde53cc9eb1eecb03d54fbaef
Ferran Borreguero <ferranbt@protonmail.com>
2023-05-24 14:51:39 +0200

    Add ENS datasource

---
+++ ethereum/data_source_ens_test.go
@@ -0,0 +1,29 @@
+package ethereum
+
+import (
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
+)
+
+func TestAccENS_basic(t *testing.T) {
+	t.Skip("cannot be tested without a forked devnet")
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:  func() { testAccPreCheck(t) },
+		Providers: testAccProviders,
+		Steps: []resource.TestStep{
+			{
+				Config: `
+				data "ethereum_ens" "resolve" {
+					name = "arachnid.eth"
+				}
+				`,
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr(
+						"data.ethereum_ens.resolve", "address", "0xb8c2C29ee19D8307cb7255e1Cd9CbDE883A267d5"),
+				),
+			},
+		},
+	})
+}
