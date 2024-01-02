43ac56c63afdebaff30f5f3c11e52b7594304217
Ferran Borreguero <ferranbt@protonmail.com>
2024-01-02 10:28:06 +0100

    Add gas_price datasource

---
+++ ethereum/data_source_gas_price.go
@@ -0,0 +1,35 @@
+package ethereum
+
+// getGasPrice
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+)
+
+func datasourceGetGasPrice() *schema.Resource {
+	return &schema.Resource{
+		ReadContext: datasourceGetGasPriceRead,
+		Schema: map[string]*schema.Schema{
+			"gas_price": {
+				Type:     schema.TypeInt,
+				Computed: true,
+			},
+		},
+	}
+}
+
+func datasourceGetGasPriceRead(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
+	gasPrice, err := m.(*client).httpClient.Eth().GasPrice()
+	if err != nil {
+		return diag.FromErr(err)
+	}
+
+	d.SetId(fmt.Sprintf("%d", gasPrice))
+	d.Set("gas_price", int(gasPrice))
+
+	return nil
+}
