6459ab700bb7b8d9a7dcace4fac4b3807bab9fa3
Ferran Borreguero <ferranbt@protonmail.com>
2024-01-04 11:48:33 +0100

    Mopre

--- examples/data-sources/ethereum_contract_code/data-source.tf
+++ examples/data-sources/ethereum_contract_code/data-source.tf
@@ -1,3 +1,8 @@
-data "contract_code" "pp" {
-	address = "x"
+data "contract_code" "deposit_contract" {
+  address = "0x00000000219ab540356cBB839Cbe05303d7705Fa"
+}
+
+// Query the contract and validate that it exists
+data "contract_code" "deposit_contract" {
+  address = "0x00000000219ab540356cBB839Cbe05303d7705Fa"
 }
