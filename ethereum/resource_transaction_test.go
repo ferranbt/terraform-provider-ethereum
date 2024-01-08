d70934fa00a3765858592662ce972ce6f19f15d3
Ferran Borreguero <ferranbt@protonmail.com>
2024-01-08 17:05:57 +0100

    Use full function name to send a transaction (#21)
    * Use full function name to send a transaction

--- ethereum/resource_transaction_test.go
+++ ethereum/resource_transaction_test.go
@@ -112,3 +112,45 @@ func TestAccTransaction_Transfer(t *testing.T) {
 		},
 	})
 }
+
+func TestAccTransaction_method_fromSignature(t *testing.T) {
+	resource.Test(t, resource.TestCase{
+		PreCheck:  func() { testAccPreCheck(t) },
+		Providers: testAccProviders,
+		Steps: []resource.TestStep{
+			{
+				Config: `
+				data "ethereum_eoa" "account" {
+					mnemonic = "test test test test test test test test test test test junk"
+				}
+				resource "ethereum_contract_deployment" "deploy" {
+					signer = data.ethereum_eoa.account.signer
+					artifact = "../testcases/out:Inputs"
+					input = [
+						"0x95222290dd7278aa3ddd389cc1e1d165cc4bafe5",
+						"1",
+						"0xcc84c3b12f6ae46a791f06a0297bb2d9e60d1d4e0f7c0aff2f5be06cea9189d4",
+						jsonencode({
+						  "number" = "1"
+						})
+					]
+				}
+				resource "ethereum_transaction" "update" {
+					signer = data.ethereum_eoa.account.signer
+					to = resource.ethereum_contract_deployment.deploy.contract_address
+					function = "applyFunc(address,uint256,bytes32,(uint256 number))"
+					input = [
+						"0x95222290dd7278aa3ddd389cc1e1d165cc4bafe6",
+						"2",
+						"0xaa84c3b12f6ae46a791f06a0297bb2d9e60d1d4e0f7c0aff2f5be06cea9189d4",
+						jsonencode({
+						  "number" = "3"
+						})
+					]
+				}
+				`,
+				Check: checkTransactionDeployed(),
+			},
+		},
+	})
+}
